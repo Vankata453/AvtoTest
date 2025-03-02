986e1b6ac8d36bc09e1dbdfc597eb860611878e6
Vankata453 <78196474+Vankata453@users.noreply.github.com>
2025-03-02 18:17:57 +0200

    `TestSetDao`: Get questions and answers by IDs, now ordered

--- app/src/main/java/com/provigz/avtotest/db/TestSetDao.kt
+++ app/src/main/java/com/provigz/avtotest/db/TestSetDao.kt
@@ -4,6 +4,9 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import androidx.room.RawQuery
+import androidx.sqlite.db.SimpleSQLiteQuery
+import androidx.sqlite.db.SupportSQLiteQuery
 import com.provigz.avtotest.db.entity.Answer
 import com.provigz.avtotest.db.entity.Property
 import com.provigz.avtotest.db.entity.TestSet
@@ -12,14 +15,56 @@ import com.provigz.avtotest.db.entity.QuestionState
 
 @Dao
 interface TestSetDao {
+    suspend fun <T> getOrderedObjectsByIDs(
+        tableName: String,
+        ids: List<Int>,
+        queryFun: suspend (SupportSQLiteQuery) -> List<T>
+    ): List<T> {
+        if (ids.isEmpty()) {
+            return emptyList()
+        }
+
+        val caseStatement = ids.mapIndexed { index, id ->
+            "WHEN id = $id THEN $index"
+        }.joinToString(separator = " ")
+
+        return queryFun(
+            SimpleSQLiteQuery(
+                query =
+                """
+                    SELECT * FROM $tableName 
+                    WHERE id IN (${ids.joinToString()}) 
+                    ORDER BY CASE $caseStatement ELSE 999 END
+                """
+            )
+        )
+    }
+
     @Query("SELECT * FROM testSet WHERE id = :id")
     suspend fun getTestSetByID(id: Int): TestSet?
-    @Query("SELECT * FROM question WHERE id IN (:questionIDs)")
-    suspend fun getQuestionsByIDs(questionIDs: List<Int>): List<Question>
     @Query("SELECT * FROM questionState WHERE testSetID = :testSetID AND questionID = :questionID")
     suspend fun getQuestionStateByIDs(testSetID: Int, questionID: Int): QuestionState
-    @Query("SELECT * FROM answer WHERE id IN (:answerIDs)")
-    suspend fun getAnswersByIDs(answerIDs: List<Int>): List<Answer>
+
+    @RawQuery
+    suspend fun getQuestionsByRawQuery(query: SupportSQLiteQuery): List<Question>
+    suspend fun getQuestionsByIDs(questionIDs: List<Int>): List<Question> {
+        return getOrderedObjectsByIDs(
+            tableName = "question",
+            ids = questionIDs
+        ) { query ->
+            getQuestionsByRawQuery(query)
+        }
+    }
+    @RawQuery
+    suspend fun getAnswersByRawQuery(query: SupportSQLiteQuery): List<Answer>
+    suspend fun getAnswersByIDs(answerIDs: List<Int>): List<Answer> {
+        return getOrderedObjectsByIDs(
+            tableName = "answer",
+            ids = answerIDs
+        ) { query ->
+            getAnswersByRawQuery(query)
+        }
+    }
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertTestSet(testSet: TestSet)
